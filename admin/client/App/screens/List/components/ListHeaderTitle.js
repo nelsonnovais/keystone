719a4a443e43594c7c1f94fb8cc3aaa4f96a1571
Nelson Novais <nelsonnovais@MBP-de-Nelson.lan>
2017-07-13 14:20:46 +0100

    list sort translated in ListSort.js

--- admin/client/App/screens/List/components/ListHeaderTitle.js
+++ admin/client/App/screens/List/components/ListHeaderTitle.js
@@ -14,7 +14,7 @@ function ListHeaderTitle ({
 
 	return (
 		<h2 className={css(classes.heading)} {...props}>
-			{title} +1
+			{title}
 			<ListSort
 				activeSort={activeSort}
 				availableColumns={availableColumns}
