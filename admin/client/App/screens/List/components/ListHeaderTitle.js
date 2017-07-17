68dc72664766bcd31ab42baf2d83e4433270fc50
Nelson Novais <nelsonnovais@MBP-de-Nelson.lan>
2017-07-17 12:21:44 +0100

    some test

--- admin/client/App/screens/List/components/ListHeaderTitle.js
+++ admin/client/App/screens/List/components/ListHeaderTitle.js
@@ -19,7 +19,7 @@ function ListHeaderTitle ({
 		'Categories': 'Categorias',
 	};
 	titulo = field[0] + ' ' + field[1];
-	if (field[2]) {
+	if (field[2] === 'Categories') {
 		console.log('entrei');
 		titulo = field[0] + ' ' + lookUptable[field[2]];
 	};
