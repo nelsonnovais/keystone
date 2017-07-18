c1901d5bc197b88f3a7d7cd7dd8d85b4e7e63f21
Nelson Novais <nelsonnovais@MBP-de-Nelson.lan>
2017-07-18 22:11:59 +0100

    initial state

--- fields/types/relationship/RelationshipFilter.js
+++ fields/types/relationship/RelationshipFilter.js
@@ -177,6 +177,7 @@ var RelationshipFilter = React.createClass({
 		const itemIconHover = selected ? 'x' : 'check';
 
 		return items.map((item, i) => {
+			console.log(lookUpTable[item.name]);
 			return (
 				<PopoutList.Item
 					key={`item-${i}-${item.id}`}
